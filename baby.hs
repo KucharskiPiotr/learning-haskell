2a4d891537334b472bee25f6c2e6ec0b21653efb
KucharskiPiotr <piotr-kucharski@outlook.com>
2018-05-05 19:34:26 +0200

    after lists comprahentions

--- baby.hs
+++ baby.hs
@@ -16,19 +16,19 @@ mergeLists list1 list2 =
 putElementOnHead element list =
     element:list
 
--- Funkcje jako operacje na listach
---    head - zwraca pierwszy element
---    tail - zwraca wszstko procz pierwszego
---    last - zwraca ostatni element
---    init - zwraca wszystko procz ostatniego
---    length - zwraca dlugosc listy
---    null - sprawdza czy lista jest pusta
---    reverse - odwraca liste
---    take x y - bierze x poczatkowych elementow listy y i zwraca je
---    drop x y - zwraca elementy listy y o indeksach powyzej x wlacznie
---    maximum/minimum - zwraca max/min z listy
---    sum, product - operacje dodawania/mnozenia elementow calej listy
---    elem - sprawdza czy element nalezy do listy (np. 4 `elem` [1,2,3] => False)
+-- Functions to operate on lists
+--    head - returns first element
+--    tail - returns list without first element
+--    last - returns last element of list
+--    init - returns list without last element
+--    length - returns length of the list 
+--    null - checks if list is empty
+--    reverse - returns list in reversed order
+--    take x y - takes x first elements of list and returns them
+--    drop x y - returns elements of y list with index bigger or equal of x
+--    maximum/minimum - returns biggest/smallest element form list
+--    sum, product - makes sum/product of elements of the list
+--    elem - checks if element is in list (eg. 4 `elem` [1,2,3] => False)
 
 makeListInRange x y =
     [x..y]
@@ -36,3 +36,40 @@ makeListInRange x y =
 makeListStepTwo x y =
     [x,x+2..y]
 
+takeElementsOfSmallList num list =
+    take num (cycle list)
+
+takeElementsOfRepeatingLists num element = 
+    take num (repeat element)
+
+replicateElementAsList count element =
+    replicate count element
+
+getEvenNumbersFromRange first last =
+    [x*2 | x <- [first..last]]
+
+getEvenNumbersFromRangeBiggerThanElement first last element =
+    [x*2 | x <- [first..last], x*2 >= element]
+
+filterList list =
+    [if x /= "SPAM" then x else "" | x <- list]
+
+-- List comprahesions has a structure as:
+-- [function | predicate of argument for that function, another one, etc.]
+-- eg. [adjective ++ " " ++ noun | adjective <- param1, noun <- param2]
+
+-- We can set variable in memory in interpreter via writing
+-- let variable = value
+-- then it is possible to work with that variable
+
+-- _ means it can be anything and we won't use it
+length' xs = sum [1 | _ <- xs]
+
+removeNonUpperCase string =
+    [c | c <- string, c `elem` ['A'..'Z']]
+
+removeNonLowerCase string =
+    [c | c <- string, c `elem` ['a'..'z']]
+
+removeOddNumbers xxs =
+    [ [x | x <- xs, even x] | xs <- xxs ]
