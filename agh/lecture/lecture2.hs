7aab7d32201ee30c19a86a6990629918187e4d7f
KucharskiPiotr <piotr-kucharski@outlook.com>
2018-05-10 17:00:27 +0200

    after break

--- agh/lecture/lecture2.hs
+++ agh/lecture/lecture2.hs
@@ -43,4 +43,38 @@ foldl' f a (x:xs) = foldl f (f a x) xs
 -- Wystarczy numerowac akcje zeby miec je po kolei
 
 -- Monady - coś ważnego xd, Interfejs pozwalający na łączenie funckji w taki sposób że funkcja
--- bierze a, zwraca b opakowane w c czy tam d
+-- bierze a, zwraca b opakowane w c czy tam d. Określa się kolejnośc wywoływania funkcji
+
+-- Przykład kompuzycji funkcji która musi mieć rzeczy po kolei:
+--      echo = getChar >>= putChar
+-- pozwala na naciśnięcie klawisza i wypisania jego id
+-- Operator >>= pozwala na stworzenie tego połączenia ( biblioteka Controls.Monad )
+
+-- Definicja typu opcjonalnego : 
+data Maybe a = Just a | Nothing
+-- Podstawowy moduł generowania porażki, albo będzie dobre rozwiązanie i po prostu się coś 
+-- zwraca bo się udało, inaczej zwróć nic xd np. dzielenie przez 0, jak sie uda to zwróć,
+-- jak nei to zwróć nic
+
+-- Można dać funkcji że zwraca 0 1 lub wiele wartości, wtedy deklaracja typów:
+-- Maybe a -> (a -> Maybe b) -> Maybe b
+-- Jeżeli gdziekolwiek po drodze wyrzucone zostanie nic to nie idź dalej tylko wyrzuć nic
+-- robi się to za pomocą operatora (>>=) - /nazwa tego operatora to bind/:
+-- (>>=) Maybe a -> (a -> Maybe b) -> Maybe b
+-- Dążymy do tego żeby rozłożyć wszystko na poszczególne kawałki 
+-- (po kolei, bułka, sałata, mięso itd a nie mielonka)
+
+-- Definicja tego operatora żeby to działało:
+-- Implementacja:
+-- Maybe a -> (a -> Maybe b) -> Maybe b
+Just a >>= f = f a
+Nothing >>= f = Nothing
+
+-- Just tworzy z tej "zmiennej" zmienną typu Maybe (a -> Maybe a)
+-- Za pomoca tego co napisalismy w wykorzystaniu f nie trzeba sprawdzac wiecej if costam == null
+-- JEst cos co może się nie udać zatem zamykamy to w funkcji i samo się sprawdza i obsługuje
+
+-- Jak cos się powtarza to tworzymy abstrakcję to towrzymy coś żeby było uporządkowane
+-- Za pomocą tego wszystkiego można utrzymać kod w dobrym stanie
+-- po więcej informacji: :i Maybe
+
