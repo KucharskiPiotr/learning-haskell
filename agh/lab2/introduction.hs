fd1999a685d354ffb1be091e6c7211513ce1b1c0
KucharskiPiotr <piotr-kucharski@outlook.com>
2018-05-10 12:07:32 +0200

    bacteria

--- agh/lab2/introduction.hs
+++ agh/lab2/introduction.hs
@@ -9,5 +9,17 @@ sign x  | x > 0 = 1
 sum2a m n =
     m + n
 
-my_zip list1 list2 =
-    [(head list1, y) | y <- list2]
+sum2b (m:n:_) = m + n
+
+my_zip :: [a] -> [b] -> [(a,b)]
+my_zip (a:list1) (b:list2) =
+    (a, b) : my_zip list1 list2
+my_zip _ _ = []
+
+
+-- Bacteria A after 1s -> 2x bact. B
+-- bacteria B after -> bact. A + bact. B
+bacteria :: Int -> (Int, Int)
+bacteria 0 = (1,0)
+bacteria time = 
+   (snd (bacteria (time-1)), (((fst (bacteria (time-1)))*2) + (snd (bacteria (time-1)))))
