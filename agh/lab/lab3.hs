4ae7392887fc253b10ffa49a2053fc048cb963af
KucharskiPiotr <piotr-kucharski@outlook.com>
2018-05-17 12:27:09 +0200

    Exercises done

--- agh/lab/lab3.hs
+++ agh/lab/lab3.hs
@@ -1,13 +1,19 @@
 
+-- Zadanie 4
 policzISumuj :: (Int -> Int) -> Int -> Int -> Int
 policzISumuj f x y =
     sum (map f [x..y])
 
+-- Zadanie 5
 pierwsze :: [Int] -> [Int]
 pierwsze a =
     filter (\x -> null [i | i <- [2..x-1], x `mod` i == 0]) a
 
+-- Zadanie 6
 conajmniejn :: [Int] -> Int -> [Int]
 conajmniejn [] _ = []
-conajmniejn [x:list] n =
-    (filter (\i -> (length [a | a <- list, a == x]) >= n) list)
+conajmniejn (x:list) n  | (length [y | y <- list, y == x]) >= (n-1) = 
+    if (x `elem` (conajmniejn list n))
+    then (conajmniejn list n)
+    else (x : (conajmniejn list n))
+                        | otherwise = conajmniejn list n
