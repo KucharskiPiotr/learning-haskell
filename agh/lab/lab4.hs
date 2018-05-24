233bf1eb47977a316005c7ac050836f96036c3bb
KucharskiPiotr <piotr-kucharski@outlook.com>
2018-05-24 15:31:56 +0200

    Exercise 3 done

--- agh/lab/lab4.hs
+++ agh/lab/lab4.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE FlexibleInstances #-}
-
 import Data.Char
+import Data.List
 
+-- Zadanie 1
 class Intable a where
     toInt :: a -> Int
 
@@ -13,3 +14,29 @@ instance Intable [Char] where
 
 mySuperAdd :: (Intable a) => a -> a -> Int
 mySuperAdd x y = toInt x + toInt y
+
+
+-- Zadanie 2
+data Osoba = Osoba 
+    {
+        imie :: String,
+        nazwisko :: String,
+        pesel :: String
+    } deriving (Show)
+
+instance Eq Osoba where
+    (==) x y = (pesel x) == (pesel y)
+
+instance Ord Osoba where
+    (<) x y = (pesel x) < (pesel y)
+    (>) x y = (pesel x) > (pesel y)
+    
+
+-- Zadanie 3
+comparePesel :: String -> Osoba -> Bool
+comparePesel str oso = str == pesel oso
+
+findID :: (a -> Bool) -> [a] -> Maybe a
+findID _ [] = Nothing
+findID lambda (x:list)  | lambda x = Just x
+                        | otherwise = findID lambda list
